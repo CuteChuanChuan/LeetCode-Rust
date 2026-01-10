e7af5f7c2036bd3008a6e36536af07270edcd0ab
Yu-Chuan Hung <yuchuanhung.raymond@gmail.com>
2026-01-10 14:53:19 +0800

    chore: Sort path in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,31 @@
 pub struct Solution;
 
-#[path = "num_0001_TwoSum.rs"]
+#[path = "num_1_TwoSum.rs"]
 mod two_sum;
 
 #[path = "num_15_3sum.rs"]
 mod three_sum;
 
+#[path = "num_36_ValidSudoku.rs"]
+mod valid_sudoku;
+
 #[path = "num_49_GroupAnagrams.rs"]
 mod group_anagrams;
 
-#[path = "num_0121_BestTimeToBuyAndSellStock.rs"]
+#[path = "num_70_ClimbingStairs.rs"]
+mod climbing_stairs;
+
+#[path = "num_121_BestTimeToBuyAndSellStock.rs"]
 mod best_time_to_buy_and_sell_stock;
+
+#[path = "num_125_ValidPalindrome.rs"]
+mod valid_palindrome;
+
+#[path = "num_128_LongestConsecutiveSequence.rs"]
+mod longest_consecutive_sequence;
+
+#[path = "num_167_TwoSumIiInputArrayIsSorted.rs"]
+mod two_sum_ii_input_array_is_sorted;
 
 #[path = "num_217_ContainsDuplicate.rs"]
 mod contains_duplicate;
@@ -18,14 +33,5 @@ mod contains_duplicate;
 #[path = "num_242_ValidAnagram.rs"]
 mod valid_anagram;
 
-#[path = "num_70_ClimbingStairs.rs"]
-mod climbing_stairs;
-
 #[path = "num_347_TopKFrequentElements.rs"]
 mod top_k_frequent_elements;
-
-#[path = "num_36_ValidSudoku.rs"]
-mod valid_sudoku;
-
-#[path = "num_128_LongestConsecutiveSequence.rs"]
-mod longest_consecutive_sequence;
